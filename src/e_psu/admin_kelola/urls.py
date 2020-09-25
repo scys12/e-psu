cb744169cbcea82287116b83793e07e2235e6a21
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-09-25 17:49:26 +0700

    move serah_terima to admin_kelola

--- src/e_psu/admin_kelola/urls.py
+++ src/e_psu/admin_kelola/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path("login", views.login_admin_kelola, name="login"),
     path("register", views.register_admin_kelola, name="register"),
     path("logout", views.logout_admin_kelola, name="logout"),
-    path("dashboard", views.index, name="index")
+    path("dashboard", views.index, name="index"),
+    path("serah_terima/tambah", views.serah_terima_tambah, name="serah_terima_tambah"),
+    path("serah_terima/tampil/<int:id>", views.serah_terima_tampil, name="serah_terima_tampil"),
+    path("serah_terima/ubah/<int:id>", views.serah_terima_ubah, name="serah_terima_ubah"),
+    path("serah_terima/hapus/<int:id>", views.serah_terima_hapus, name="serah_terima_hapus"),
 ]
--- src/e_psu/admin_kelola/views.py
+++ src/e_psu/admin_kelola/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import logout
 from .forms import AdminKelolaRegistrationForm
+from serah_terima.forms import DokumenForm
 from account.forms import RegisterForm, LoginForm
 from django.db import transaction
 from serah_terima.models import Dokumen
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from account.decorators import admin_kelola_required, anonymous_required
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from laporan.models import BerkasLaporan
 # Create your views here.
@@ -71,3 +73,65 @@ def login_admin_kelola(request):
 def logout_admin_kelola(request):
     logout(request)
     return redirect('home')
+
+@login_required(login_url='/admin_kelola/login')
+@admin_kelola_required
+def serah_terima_tambah(request):    
+    if request.method == 'POST':
+        form = DokumenForm(request.POST, request.FILES)
+        if form.is_valid():
+            admin = AdminKelola.objects.get(pk=request.user.id)
+            form_serah_terima = form.save(commit=False)
+            form_serah_terima.admin_kelola = admin
+            form_serah_terima.save()
+            nama_psu = form.cleaned_data.get('nama_psu')
+            messages.success(request, f'Dokumen berhasil ditambahkan.', extra_tags='serah_terima')
+            return redirect('admin_kelola:index')
+    else:
+        form = DokumenForm()
+    return render(request, "serah_terima/tambah.html", {
+        'form' : form
+    })
+
+@login_required(login_url='/admin_kelola/login')
+@admin_kelola_required
+def serah_terima_tampil(request, id):
+    dokumen = Dokumen.objects.get(id=id)
+    return render(request, "serah_terima/tampil.html", {
+        'dokumen' : dokumen
+    })
+
+@login_required(login_url='/admin_kelola/login')
+@admin_kelola_required
+def serah_terima_ubah(request, id):
+    try:
+        dokumen = Dokumen.objects.get(id = id)
+    except Dokumen.DoesNotExist:
+        return redirect('admin_kelola:index')
+    
+    form = DokumenForm(request.POST or None, request.FILES or None, instance = dokumen)
+
+    if form.is_valid():
+        admin = AdminKelola.objects.get(pk=request.user.id)
+        form_serah_terima = form.save(commit=False)
+        form_serah_terima.admin_kelola = admin
+        form_serah_terima.save()
+        messages.success(request, f'Dokumen berhasil diperbarui.', extra_tags='serah_terima')
+        return redirect('admin_kelola:index')
+
+    print(form.errors)
+    return render(request, "serah_terima/ubah.html", {
+        'form' : form, 'dokumen' : dokumen
+    })
+
+@login_required(login_url='/admin_kelola/login')
+@admin_kelola_required
+def serah_terima_hapus(request, id):
+    try:
+        dokumen = Dokumen.objects.get(id=id)
+    except Dokumen.DoesNotExist:
+        return redirect('admin_kelola:index')
+
+    dokumen.delete()
+    messages.success(request, f'Dokumen berhasil dihapus.', extra_tags='serah_terima')
+    return redirect('admin_kelola:index')
