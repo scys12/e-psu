d3c9eabccea3591a7ee4ecca6826ea574a63e15e
Samuel S <sinambelasam03@gmail.com>
2020-09-30 22:43:27 +0700

    fix bug user id inserted in another table

--- src/e_psu/admin_kelola/models.py
+++ src/e_psu/admin_kelola/models.py
@@ -3,14 +3,14 @@ from account.models import Account
 from django.db.models.signals import post_save
 # Create your models here.
 class AdminKelola(models.Model):
-    user = models.OneToOneField(Account,on_delete=models.CASCADE, primary_key = True)
+    user = models.OneToOneField(Account,on_delete=models.CASCADE)
     nama = models.CharField(max_length=30)
 
     class Meta:
         db_table= "kelola_adminkelola"
     
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            profile, created = AdminKelola.objects.get_or_create(user=instance)
+    # def create_user_profile(sender, instance, created, raw=False, **kwargs):
+    #     if created and not raw:
+    #         profile = AdminKelola.objects.create(user=instance)
     
-    post_save.connect(create_user_profile, sender=Account)
+    # post_save.connect(create_user_profile, sender=Account)
--- src/e_psu/warga/models.py
+++ src/e_psu/warga/models.py
@@ -4,7 +4,7 @@ from django.core.validators import RegexValidator
 from django.db.models.signals import post_save
 # Create your models here.
 class Warga(models.Model):
-    user = models.OneToOneField(Account,on_delete=models.CASCADE, primary_key = True)
+    user = models.OneToOneField(Account,on_delete=models.CASCADE)
     no_ktp = models.CharField(validators=[RegexValidator(regex='\d{16}$', message='KTP harus mempunyai 16 digit angka')], unique=True, max_length=16)
     nama = models.CharField(max_length=30)
     alamat_rumah = models.CharField(max_length=100)
@@ -12,8 +12,8 @@ class Warga(models.Model):
     class Meta:
         db_table= "kelola_warga"
     
-    def create_user_profile(sender, instance, created, **kwargs):  
-        if created:            
-            profile, created = Warga.objects.get_or_create(user=instance)
+    # def create_user_profile(sender, instance, created, **kwargs):  
+    #     if created:            
+    #         profile, created = Warga.objects.get_or_create(user=instance)
 
-    post_save.connect(create_user_profile, sender=Account)
+    # post_save.connect(create_user_profile, sender=Account)
